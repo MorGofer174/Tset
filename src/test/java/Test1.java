3287ec4e365abf999925b0c56ac6ef6bb1f6888d
morgofer174 <morgofer174@gmail.com>
2021-07-02 15:46:44 +0300

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("x");
+        Test1.Mor();
+    }
+    public static void Mor(){
+        System.out.println("mor");
+    }
+}
